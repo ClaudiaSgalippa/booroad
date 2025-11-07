62536969aba689cf15c7cb162db05470a2d6fa2b
fedstrial <fedstrial@proton.me>
2025-11-07 18:38:55 +0100

    update layout

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -4,15 +4,17 @@ export default function MyHeader() {
 	const location = useLocation();
 
 	return (
-		<header className="d-flex justify-content-between align-items-center my-4">
-			<h1 className="display-1">
-				<Link to="/">BooRoad</Link>
-			</h1>
-			{location.pathname === "/users" && (
-				<Link to="/" className="btn btn-primary">
-					Torna alla Homepage
-				</Link>
-			)}
+		<header className="bg-light">
+			<div className="container-wide py-3 d-flex justify-content-between align-items-center">
+				<h1 className="display-1">
+					<Link to="/">BooRoad</Link>
+				</h1>
+				{location.pathname === "/users" && (
+					<Link to="/" className="btn btn-primary">
+						Torna alla Homepage
+					</Link>
+				)}
+			</div>
 		</header>
 	);
 }
